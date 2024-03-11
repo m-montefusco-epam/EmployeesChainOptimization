b74ddfb2283b248d30536392463900c31d55027e
m-montefusco-epam <massimo_montefusco@epam.com>
2024-03-11 12:31:33 +0100

    First commit demo project Employees Chain Optimization

--- src/main/java/EmployeesChainOptimization/readers/Reader.java
+++ src/main/java/EmployeesChainOptimization/readers/Reader.java
@@ -1,6 +1,5 @@
 package EmployeesChainOptimization.readers;
 
-import EmployeesChainOptimization.exceptions.ReaderException;
 import EmployeesChainOptimization.model.Employee;
 
 import java.util.List;
